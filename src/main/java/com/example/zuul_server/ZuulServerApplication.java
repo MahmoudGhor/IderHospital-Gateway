1054fe93937fca70d842088b52fb1c87351b6492
Mahmoud <mahmoud.ghorbel@esprit.tn>
2019-11-13 22:22:30 +0000

    coorection

--- src/main/java/com/example/zuul_server/ZuulServerApplication.java
+++ src/main/java/com/example/zuul_server/ZuulServerApplication.java
@@ -6,6 +6,7 @@ import com.example.zuul_server.repositories.RoleRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
@@ -13,6 +14,7 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableZuulProxy
+@EnableDiscoveryClient
 public class ZuulServerApplication {
     @Bean
     CommandLineRunner init(RoleRepository roleRepository) {
